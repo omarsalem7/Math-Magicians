319391433bd731d445e958fa64d1d488daa8125a
Omar <omarsalem0721@gmail.com>
2022-02-16 10:53:00 +0200

    Fix LF and CRLF

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import Calculator from './components/Calculator';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
+    this.state = {};
   }
 
   render() {
-    const { data } = this.state;
     return (
       <div className="App">
-        <Calculator data={data} />
+        <Calculator />
       </div>
     );
   }
